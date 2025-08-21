f6655a43231bdd80bd2ae558ac3272e0176bb029
Rafaela Johnson <rafaelajohnson1@gmail.com>
2025-08-20 22:28:53 -0300

    fixes

--- db/client.js
+++ db/client.js
@@ -1,3 +1,10 @@
+import "dotenv/config";        // must be line 1
 import pg from "pg";
-const db = new pg.Client(process.env.DATABASE_URL);
+
+// Support DATABASE_URL or PG* split vars (either works)
+const { DATABASE_URL, PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE } = process.env;
+const db = DATABASE_URL
+  ? new pg.Client({ connectionString: DATABASE_URL })
+  : new pg.Client({ user: PGUSER, password: PGPASSWORD, host: PGHOST, port: PGPORT, database: PGDATABASE });
+
 export default db;
